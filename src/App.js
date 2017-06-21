7a6fad094071ac1c58c0bf35e581fcc0437ec261
fancn <fanyj@yikeer.com>
2017-06-21 21:37:45 +0800

    second commit with initial start

--- src/App.js
+++ src/App.js
@@ -8,14 +8,22 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      number: 1
+      number: 1,
+      number2: 0
     }
     this.onChange = this.onChange.bind(this);
+    this.onChange2 = this.onChange2.bind(this);
   }
 
   onChange = () => {
     this.setState({
       number: parseInt(this.textInput.value) || 0
+    })
+  }
+
+  onChange2 = () => {
+    this.setState({
+      number2: parseInt(this.textInput2.value) || 0
     })
   }
 
@@ -29,14 +37,21 @@ class App extends Component {
             onChange={this.onChange}
             value={this.state.number}
           />
-          元 = {this.state.number * 10} 角
+          元
+          <input
+            type='text'
+            ref={(input) => { this.textInput2 = input; }}
+            onChange={this.onChange2}
+            value={this.state.number2}
+          />
+          角 = {this.state.number * 10 + this.state.number2} 角
         </h3>
         <div>
           <Yuan number={this.state.number}/>
         </div>
         <div>
           {
-            _.range(this.state.number * 10).map((val, idx) => {
+            _.range(this.state.number * 10 + this.state.number2).map((val, idx) => {
               return <Jiao key={val}/>
             })
           }
