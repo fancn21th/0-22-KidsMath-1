6342938a29e7f50a7e7023c9b8a8f9091f62f882
fancn <fanyj@yikeer.com>
2017-06-21 21:26:07 +0800

    second commit with initial start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
-import Yuan from './components/presentaion/yuan'
-import Jiao from './components/presentaion/jiao'
+import _ from 'lodash'
+import Yuan from './components/presentaion/yuan';
+import Jiao from './components/presentaion/jiao';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      number: 1
+    }
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange = () => {
+    this.setState({
+      number: parseInt(this.textInput.value) || 0
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h3>
+          <input
+            type='text'
+            ref={(input) => { this.textInput = input; }}
+            onChange={this.onChange}
+          />
           元 与 角
         </h3>
         <div>
-          <Yuan />
+          <Yuan number={this.state.number}/>
         </div>
         <div>
           {
-            [1,2,3,4,5,6,7,8,9,10,11,12].map((val, idx) => {
-              return val % 6 === 0 ? <br/> : <Jiao />
+            _.range(this.state.number * 10).map((val, idx) => {
+              return <Jiao key={val}/>
             })
           }
         </div>
--- src/components/presentaion/yuan/index.js
+++ src/components/presentaion/yuan/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Yuan = () => (
+const Yuan = ({number}) => (
   <svg width='100' height='100'>
     <circle cx='50' cy='50' r='40' stroke='green' stroke-width='4' fill='silver' />
     <text x='50%' y='50%' text-anchor='middle' stroke='black' stroke-width='2px' dy='8' dx='-8'>
-      1元
+      {number}元
     </text>
   </svg>
 )
